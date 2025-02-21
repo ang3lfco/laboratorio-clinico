5151067cbe515f49d35f12f8b306df09577af176
ang3lfco <ang3lfco@outlook.com>
2025-02-21 12:10:58 -0700

    inicializacion de proyecto

---
+++ laboratorio-clinico/src/main/java/com/mycompany/laboratorio/clinico/LaboratorioClinico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.laboratorio.clinico;
+
+/**
+ *
+ * @author ang3lfco
+ */
+public class LaboratorioClinico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
